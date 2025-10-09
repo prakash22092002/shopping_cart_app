1a498a737865e72b739c45036e17d2a22b0e95cf
Prakash Paudel <p.prakashpaudel35@gmail.com>
2025-10-09 23:03:49 +0530

    feat(api): created fetch allProducts and fetchProductById function.

---
+++ src/api/products.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const API_BASE = 'https://catalog-management-system-dev-ak3ogf6zea-uc.a.run.app';
+
+export const fetchProductsAPI = async ({ page = 1, limit = 10, search = '', category = '', sort = '', signal }) => {
+    try {
+        const params = new URLSearchParams({
+            page,
+        });
+
+        const url = `${API_BASE}/cms/products?${params}`;
+        console.log('Making API request to:', url);
+
+        const response = await axios.get(url, { signal });
+
+        debugger
+        console.log(' API Response received:', response.data);
+
+        return response.data;
+    } catch (error) {
+        console.error('Error response:', error.response?.data);
+        throw error;
+    }
+};
+
+
+// yae mae create keya just for the future purpose , fro retrive data end point for the fetch unique products by uska id
+export const fetchProductByIdAPI = async (id, signal) => {
+    const response = await axios.get(`${API_BASE}/cms/products/${id}`, { signal });
+    return response.data;
+};
