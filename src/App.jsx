2f25ab2210a5c98d6431bf303736ded6a311045e
Prakash Paudel <p.prakashpaudel35@gmail.com>
2025-10-09 22:30:37 +0530

    feat: set up the Routes and implemented the lazy loading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const ProductTable = lazy(() => import('./pages/ProductTable'));
+const ProductDetail = lazy(() => import('./pages/ProductDetail'));
 
-export default App
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<CircularProgress sx={{ display: 'block', mx: 'auto', mt: 4 }} />}>
+      <Routes>
+        <Route path='/' element={<ProductTable />} />
+        <Route path='/product/:id' element={<ProductDetail />} />
+      </Routes>
+    </Suspense>
+  </Router>
+);
+
+
+export default App;
